71962cdaed6e99227b970ed9e4f6c621727371f5
Avinoam <eranis54321@gmail.com>
2024-12-20 22:52:51 +0200

    Updates

---
+++ precache-manifest.f5fbdf723197eba639405c37476ecacd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc9f2ef06fba7e7323f313a5763ec18",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "094e69de52ebb1414cfc",
+    "url": "/my-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d18dd438acaa46b9c3b1",
+    "url": "/my-portfolio/static/css/main.1088719a.chunk.css"
+  },
+  {
+    "revision": "094e69de52ebb1414cfc",
+    "url": "/my-portfolio/static/js/2.c0c660fc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-portfolio/static/js/2.c0c660fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18dd438acaa46b9c3b1",
+    "url": "/my-portfolio/static/js/main.da920d74.chunk.js"
+  },
+  {
+    "revision": "28d97e6c4183a7c87d51",
+    "url": "/my-portfolio/static/js/runtime-main.9994cf37.js"
+  }
+]);
