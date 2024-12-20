fa44bea2deb9d69510f34aaf4d54544cdb08c201
Avinoam <eranis54321@gmail.com>
2024-12-20 11:01:03 +0200

    Updates

---
+++ precache-manifest.f13434b84935f3cf608b612d7286de8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e95d6f34ebe5ce4cfa8333586ecf75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f202d34d5e31786087",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0588ea107a433e53645c",
+    "url": "/static/css/main.79ee339c.chunk.css"
+  },
+  {
+    "revision": "52f202d34d5e31786087",
+    "url": "/static/js/2.671566d9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.671566d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0588ea107a433e53645c",
+    "url": "/static/js/main.20287662.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
